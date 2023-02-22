35eb859bb123030fbf962fd583be77970f357b2f
Joel Inyang <joelinyang2003@gmail.com>
2023-02-22 08:51:21 +0100

    note

--- Noteproject/Noteproject/settings.py
+++ Noteproject/Noteproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-^h2=y+zl@85jal4i99632(iw=fpk(wwh$&o=(jr41b*0z3^euk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
